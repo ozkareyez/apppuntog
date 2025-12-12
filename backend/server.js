3677aee3c175cf6eb27ca9ae5ad167a52265f68f
ozkareyez <ozkareyez@gmail.com>
2025-12-12 13:44:12 -0500

    poll2

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ const DB = mysql.createPool({
   host: process.env.DB_HOST || process.env.MYSQLHOST || "localhost",
   user: process.env.DB_USER || process.env.MYSQLUSER || "",
   password: process.env.DB_PASSWORD || process.env.MYSQLPASSWORD || "",
-  database: process.env.DB_NAME || process.env.MYSQLDATABASE || "tiendapuntog",
+  database: process.env.DB_NAME || process.env.MYSQLDATABASE || "tienda",
   port: process.env.DB_PORT || process.env.MYSQLPORT || 3306,
 });
 
