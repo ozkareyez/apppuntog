fc6e3bea859f95e09a459a1fa2a620368ebe4cb6
ozkareyez <ozkareyez@gmail.com>
2025-12-12 11:46:26 -0500

    url3

--- puntog/src/componentes/Navbar/header/Cards.jsx
+++ puntog/src/componentes/Navbar/header/Cards.jsx
@@ -36,7 +36,7 @@ const Cards = () => {
     try {
       setLoading(true);
       const response = await fetch(
-        "gleaming-motivation-production-4018.up.railway.app/productos"
+        "mysql-production-44c5.up.railway.app/productos"
       );
       if (!response.ok) {
         throw new Error("error al obtener datos del servidor");
