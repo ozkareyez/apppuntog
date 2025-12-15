ae16daea5451beb9226179822d476542165032d8
ozkareyez <ozkareyez@gmail.com>
2025-12-15 10:34:12 -0500

    import url

--- puntog/src/componentes/ContactForm.jsx
+++ puntog/src/componentes/ContactForm.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Config } from "../config.js";
+import API_URL from "../config";
 
 const ContactForm = () => {
   const [formData, setFormData] = useState({
