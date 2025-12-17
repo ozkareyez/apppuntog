7631cb4faf901265f3754f265603fc660d8ce828
ozkareyez <ozkareyez@gmail.com>
2025-12-17 17:56:24 -0500

    pantalla negra

--- puntog/src/componentes/PublicLayout.jsx
+++ puntog/src/componentes/PublicLayout.jsx
@@ -1,9 +1,9 @@
-import { Outlet } from "react-router-dom";
-
 export default function PublicLayout() {
   return (
-    <div className="min-h-screen">
+    <>
+      <Navbar />
       <Outlet />
-    </div>
+      <Foter />
+    </>
   );
 }
