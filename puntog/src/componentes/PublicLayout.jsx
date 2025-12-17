ed1e6f0280a586d6cd8d78d8179dc934f95b0aff
ozkareyez <ozkareyez@gmail.com>
2025-12-17 18:11:24 -0500

    pantalla negra vol 2

--- puntog/src/componentes/PublicLayout.jsx
+++ puntog/src/componentes/PublicLayout.jsx
@@ -1,3 +1,9 @@
+import { Outlet } from "react-router-dom";
+
+// IMPORTS COMPONENTES
+import Navbar from "./Navbar/Navbar"; // Asegúrate que la ruta sea correcta
+import Foter from "./Foter"; // Si Foter está en la misma carpeta "componentes"
+
 export default function PublicLayout() {
   return (
     <div className="min-h-screen flex flex-col bg-black text-white">
