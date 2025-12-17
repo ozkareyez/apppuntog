f3a5fd0568072a5aa4f8ca8ec8d863d69b368962
ozkareyez <ozkareyez@gmail.com>
2025-12-17 12:01:01 -0500

    hola ojala ya

--- puntog/src/App.jsx
+++ puntog/src/App.jsx
@@ -2,12 +2,10 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Home from "./componentes/Home";
 import Cards from "./componentes/Navbar/header/Cards";
-
 import Login from "./admin/Login";
 import Dashboard from "./admin/dashboard/Dashboard";
 import ContactosAdmin from "./admin/ContactosAdmin";
 import PedidosAdmin from "./admin/PedidosAdmin";
-
 import AdminLayout from "./admin/dashboard/AdminLayout";
 import PublicLayout from "./componentes/PublicLayout";
 import ProtectedRoute from "./admin/ProtectedRoute";
@@ -16,24 +14,25 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* 🌐 Público */}
+        {/* Público */}
         <Route element={<PublicLayout />}>
           <Route path="/" element={<Home />} />
           <Route path="/catalogo" element={<Cards />} />
         </Route>
 
-        {/* 🔁 /admin → login */}
-        <Route path="/admin" element={<Navigate to="/admin/login" replace />} />
-
-        {/* 🔐 Login */}
+        {/* Admin */}
         <Route path="/admin/login" element={<Login />} />
+        <Route
+          path="/admin"
+          element={<Navigate to="/admin/dashboard" replace />}
+        />
 
-        {/* 🔒 Admin protegido */}
         <Route element={<ProtectedRoute />}>
           <Route path="/admin" element={<AdminLayout />}>
+            <Route index element={<Dashboard />} />
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="pedidos" element={<PedidosAdmin />} />
-            <Route path="contactos" element={<ContactosAdmin />} />
+            <Route path="contacto" element={<ContactosAdmin />} />
           </Route>
         </Route>
       </Routes>
--- puntog/src/admin/ProtectedRoute.jsx
+++ puntog/src/admin/ProtectedRoute.jsx
@@ -1,11 +1,11 @@
-import { Navigate } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 
-export default function ProtectedRoute({ children }) {
+export default function ProtectedRoute() {
   const isAuth = localStorage.getItem("admin_auth") === "yes";
 
   if (!isAuth) {
     return <Navigate to="/admin/login" replace />;
   }
 
-  return children;
+  return <Outlet />;
 }
