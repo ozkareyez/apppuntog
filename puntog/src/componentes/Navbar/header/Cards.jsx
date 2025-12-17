f4f8735b30eaf029ae89f9b77ceab52a9f2dfcc0
ozkareyez <ozkareyez@gmail.com>
2025-12-17 16:45:27 -0500

    dos cards moivil

--- puntog/src/componentes/Navbar/header/Cards.jsx
+++ puntog/src/componentes/Navbar/header/Cards.jsx
@@ -1120,7 +1120,7 @@ ${cart
           Nuestros Productos
         </h1>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+        <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
           {productos.map((p) => (
             <div
               key={p.id}
