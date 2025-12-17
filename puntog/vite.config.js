fbce9d919ee48e258cb6d52c621ca5f9eba42cb3
ozkareyez <ozkareyez@gmail.com>
2025-12-17 17:39:12 -0500

    viteconfig

--- puntog/vite.config.js
+++ puntog/vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import { fileURLToPath, URL } from "node:url";
+import path from "path";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@": path.resolve(__dirname, "src"),
     },
   },
 });
