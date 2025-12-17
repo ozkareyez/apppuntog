cd64168c594ed08f92c7ed9f6d412f665dd98f44
ozkareyez <ozkareyez@gmail.com>
2025-12-17 10:12:53 -0500

    adminLayout

--- puntog/src/App.jsx
+++ puntog/src/App.jsx
@@ -23,7 +23,7 @@ export default function App() {
         {/* 🔐 Login */}
         <Route path="admin" element={<Login />}>
           {/* <Route path="/admin/Login" element={<Login />} /> */}
-          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="dashboard" element={<AdminLayout />} />
           <Route path="pedidos" element={<PedidosAdmin />} />
           <Route path="contactos" element={<ContactosAdmin />} />
         </Route>
