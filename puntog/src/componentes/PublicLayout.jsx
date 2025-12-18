eca49f27bd24c5827cb0b71321fd6e1d63f684da
ozkareyez <ozkareyez@gmail.com>
2025-12-18 07:48:13 -0500

    outel en public

--- puntog/src/componentes/PublicLayout.jsx
+++ puntog/src/componentes/PublicLayout.jsx
@@ -1,11 +1,12 @@
+import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar/Navbar";
 
 export default function PublicLayout() {
   return (
-    <div className="min-h-screen flex flex-col bg-white text-black">
+    <div className="min-h-screen bg-white text-black">
       <Navbar />
 
-      <main className="flex-1">
+      <main className="p-6">
         <Outlet />
       </main>
     </div>
