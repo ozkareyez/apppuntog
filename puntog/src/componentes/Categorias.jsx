3dc55d50eda8e43fc417ca4e76c98cf9af71e0ed
ozkareyez <ozkareyez@gmail.com>
2025-12-18 12:04:02 -0500

    categorias en el map de clase

--- puntog/src/componentes/Categorias.jsx
+++ puntog/src/componentes/Categorias.jsx
@@ -28,7 +28,7 @@ const Categorias = () => {
 
         {/* GRID */}
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
-          {clases.map((clases, i) => (
+          {clases.map((clase, i) => (
             <div
               key={i}
               className="
