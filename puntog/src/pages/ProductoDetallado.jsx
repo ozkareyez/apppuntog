20dd366e9943ed9899c2245a41673fc447e542f3
ozkareyez <ozkareyez@gmail.com>
2025-12-20 17:29:00 -0500

    imagen tamalño

--- puntog/src/pages/ProductoDetallado.jsx
+++ puntog/src/pages/ProductoDetallado.jsx
@@ -148,7 +148,7 @@ const ProductoDetallado = () => {
             )}
 
             {/* IMAGEN PRINCIPAL */}
-            <div className="aspect-square md:aspect-auto md:h-[400px] lg:h-[500px]">
+            <div className="aspect-square md:aspect-auto md:h-[400px] lg:h-[550px]">
               <img
                 src={getImageSrc(producto.imagen)}
                 alt={producto.nombre}
