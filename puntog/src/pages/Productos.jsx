3bb9c6b8fc330f0a763ae1779c9d21e21881b708
ozkareyez <ozkareyez@gmail.com>
2025-12-22 17:02:40 -0500

    ajuste imagenes

--- puntog/src/pages/Productos.jsx
+++ puntog/src/pages/Productos.jsx
@@ -462,11 +462,11 @@ const Productos = () => {
                 )}
 
                 {/* IMAGEN */}
-                <div className="h-48 overflow-hidden">
+                <div className="h-48 w-full overflow-hidden bg-black/20">
                   <img
                     src={getImageSrc(producto.imagen)}
                     alt={producto.nombre}
-                    className="w-full h-full object-cover group-hover:scale-105 transition-transform"
+                    className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105"
                   />
                 </div>
 
