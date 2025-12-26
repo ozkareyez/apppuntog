6c22abb9da5f65a577d1db663879f2ef9b1014dc
ozkareyez <ozkareyez@gmail.com>
2025-12-26 12:28:14 -0500

    ajstadno

--- puntog/src/utils/calcularEnvio.js
+++ puntog/src/utils/calcularEnvio.js
@@ -1,5 +1,5 @@
 export function calcularEnvio({ ciudad, total }) {
-  if (total >= 200000) return 0;
+  if (total >= 200000) return "Gratis";
 
   if (!ciudad) return "Gratis en compras mayores a $200.000";
 
