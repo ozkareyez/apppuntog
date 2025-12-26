1374f380d39048e289f6f2f8b67c0a534daf52da
ozkareyez <ozkareyez@gmail.com>
2025-12-26 12:53:42 -0500

    AJUSTE DE CONSULTA

--- puntog/src/admin/PedidosAdmin.jsx
+++ puntog/src/admin/PedidosAdmin.jsx
@@ -104,7 +104,8 @@ export default function PedidosAdmin() {
                 <td>{p.telefono}</td>
                 <td>{p.direccion}</td>
                 <td>{p.departamento_nombre || "—"}</td>
-                <td>{p.ciudad || "—"}</td>
+                <td>{p.ciudad_nombre || "—"}</td>
+
                 <td className="font-semibold">
                   ${Number(p.total).toLocaleString()}
                 </td>
