10c0611e28e9bdb7772776b8068963550db6f18a
ozkareyez <ozkareyez@gmail.com>
2025-12-26 12:25:14 -0500

    ajsute de envio

--- puntog/src/utils/calcularEnvio.js
+++ puntog/src/utils/calcularEnvio.js
@@ -1,7 +1,7 @@
 export function calcularEnvio({ ciudad, total }) {
   if (total >= 200000) return 0;
 
-  if (!ciudad) return 0;
+  if (!ciudad) return "Gratis en compras mayores a $200.000";
 
   return ciudad.toLowerCase() === "cali" ? 5000 : 16000;
 }
