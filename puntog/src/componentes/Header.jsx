63a1c3814647fb9ac6b0b0a7306dfd48cb576af2
ozkareyez <ozkareyez@gmail.com>
2025-12-27 16:31:04 -0500

    logo

--- puntog/src/componentes/Header.jsx
+++ puntog/src/componentes/Header.jsx
@@ -34,7 +34,7 @@ export default function Header() {
             <img
               src="/imagenes/logo.png"
               alt="Punto G"
-              className="h-12 w-auto hover:scale-105 transition-transform"
+              className="h-30 w-auto hover:scale-105 transition-transform"
             />
           </Link>
 
