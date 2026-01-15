c0dc657b4555fd5779ca2bbc8da11ba1324946db
ozkareyez <ozkareyez@gmail.com>
2026-01-15 13:15:55 -0500

    calcular envio en formularo nuevo

--- puntog/src/componentes/Header.jsx
+++ puntog/src/componentes/Header.jsx
@@ -186,7 +186,7 @@ export default function Header() {
               {totalItems > 0 && (
                 <>
                   <span
-                    className={`absolute -top-1 -right-1 text-xs font-bold w-5 h-5 
+                    className={`absolute -top-1 -right-1 text-xs font-bold w-5 h-5
                                rounded-full flex items-center justify-center animate-pulse
                                ${
                                  scrolled
