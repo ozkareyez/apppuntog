6414ed1d49af27bfe2babeae52bceac67454c2ed
ozkareyez <ozkareyez@gmail.com>
2026-01-20 14:27:55 -0500

    canbio en linear

--- puntog/src/pages/Productos.jsx
+++ puntog/src/pages/Productos.jsx
@@ -244,7 +244,7 @@ const Productos = () => {
   );
 
   return (
-    <section className="min-h-screen bg-gradient-to-b from-white to-gray-50 py-12">
+    <section className="min-h-screen bg-linear-to-b from-white to-gray-50 py-12">
       <div className="max-w-7xl mx-auto px-4 mb-8">
         <div className="text-center mb-6 pt-15">
           <motion.h1
